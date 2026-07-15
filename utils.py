db1b803b9202db24d1dae2f24d015bf8ea31f061
erceliksari-hash <erceliksari@gmail.com>
2026-07-15 17:03:02 +0200

    Add initial structure for analiz_et function

---
+++ utils.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def analiz_et(df, sembol):
+    # Ana kodunuzdaki 'donusum_noktalari_bul' fonksiyonunu buraya yapıştırın.
+    # importlarınızı yukarıda tanımladığınızdan emin olun.
+    pass
